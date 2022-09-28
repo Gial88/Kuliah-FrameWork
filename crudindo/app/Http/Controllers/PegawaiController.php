2266aefd486152460679a722e4d126761469bb65
Gial88 <muhammad.basith18@gmail.com>
2022-09-29 07:40:53 +0800

    crudindo

---
+++ crudindo/app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pegawai;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pegawai = DB::table("pegawais")->get();
+
+        return view('index',['pegawai' => $pegawai]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function tambah()
+    {
+        return view('tambah');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // insert data ke table pegawai
+		DB::table('pegawais')->insert([
+			'pegawai_nama' => $request->nama,
+			'pegawai_jabatan' => $request->jabatan,
+			'pegawai_umur' => $request->umur,
+			'pegawai_alamat' => $request->alamat
+		]);
+		// alihkan halaman ke halaman pegawai
+		return redirect('/pegawai');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // mengambil data pegawai berdasarkan id yang dipilih
+		$pegawai = DB::table('pegawais')->where('id',$id)->get();
+		// passing data pegawai yang didapat ke view edit.blade.php
+		return view('edit',['pegawai' => $pegawai]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pegawai $pegawai)
+    {
+        DB::table('pegawais')->where('id',$request->id)->update([
+			'pegawai_nama' => $request->nama,
+			'pegawai_jabatan' => $request->jabatan,
+			'pegawai_umur' => $request->umur,
+			'pegawai_alamat' => $request->alamat
+		]);
+		// alihkan halaman ke halaman pegawai
+		return redirect('/pegawai');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function hapus($id)
+    {
+        // menghapus data pegawai berdasarkan id yang dipilih
+		DB::table('pegawais')->where('id',$id)->delete();
+
+		// alihkan halaman ke halaman pegawai
+		return redirect('/pegawai');
+    }
+}
